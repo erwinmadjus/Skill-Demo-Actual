c8b281ea75a62ab784b4070dd68eb812057856ce
Erwin Madjus <erwinmadjusbusiness@gmail.com>
2022-02-04 17:10:14 -0800

    test

--- SDemo.java
+++ SDemo.java
@@ -1,1 +1,10 @@
-public class SDemo {}
+public class SDemo {
+    public static int add(int n, int m) {
+        if (n > m) {
+            return n + m;
+        }
+        else if (n < m) {
+            return m - n;
+        }
+        else return 10;
+}
---
+++ SDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(4, SDemo.add(4,1));
+    }
+}
